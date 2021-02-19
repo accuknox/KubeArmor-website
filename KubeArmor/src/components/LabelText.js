3f36d66cf9cca2beebed896a3316c20196222e13
ankita10119 <ankitatripathi.mp@gmail.com>
2021-02-20 02:44:15 +0530

    root source added directly

--- KubeArmor/src/components/LabelText.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LabelText = ({ children, className = '' }) => (
-  <p className={`uppercase tracking-wider ${className}`}>{children}</p>
-);
-
-export default LabelText;
