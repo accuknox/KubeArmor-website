ced61d6fce9320dd250e4d79b7e50256e24d6671
Moghankumar <52576220+Moghankumar06@users.noreply.github.com>
2021-02-20 14:16:48 +0530

    Kubearmor Home landing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
     {
       resolve: `gatsby-theme-codebushi`,
       options: {
