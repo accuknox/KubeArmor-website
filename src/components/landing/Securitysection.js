3375c2d22d5025b7b4de5389d959f7c1a505f069
Moghankumar <52576220+Moghankumar06@users.noreply.github.com>
2021-02-21 14:41:33 +0530

    Changes in Home page

--- src/components/landing/Securitysection.js
+++ src/components/landing/Securitysection.js
@@ -8,7 +8,7 @@ const Securitysection = () => {
         id="services"
         primarySlot={
           <div className="lg:pr-32 xl:pr-48">
-            <h2 className="text-3xl font-bold leading-tight">
+            <h2 className="text-3xl font-bold leading-tight securityprimarytitle">
               KubeArmor Container-aware Runtime Security Enforcement System
             </h2>
           </div>
@@ -16,19 +16,19 @@ const Securitysection = () => {
         secondarySlot={
           <div>
             <div className="mt-5">
-              <h3 className="text-3xl font-semibold leading-tight">
+              <h3 className="text-3xl font-semibold leading-tight securitytitle">
                 Effortless semantics for policy definitions
               </h3>
-              <p className="mt-2 font-light ">
+              <p className="mt-2 font-light securitydes">
                 KubeArmor provides the ability to monitor the life cycles of containers' processes
                 and take policy decisions based on them.
               </p>
             </div>
             <div className="mt-5">
-              <h3 className="text-3xl font-semibold leading-tight">
+              <h3 className="text-3xl font-semibold leading-tight securitytitle">
                 Support network security enforcement among containers
               </h3>
-              <p className="mt-2 font-light">
+              <p className="mt-2 font-light securitydes">
                 KubeArmor aims to protect containers themselves rather than interactions among
                 containers. However, using KubeArmor a user can add policies which could apply
                 policy settings at network system calls thus controlling interactions among
@@ -36,19 +36,19 @@ const Securitysection = () => {
               </p>
             </div>
             <div className="mt-5">
-              <h3 className="text-3xl font-semibold leading-tight">
+              <h3 className="text-3xl font-semibold leading-tight securitytitle">
                 Produce container-aware audit logs
               </h3>
-              <p className="mt-2 font-light">
+              <p className="mt-2 font-light securitydes">
                 Kubearmor has a eBPF engine to couple audit info with k8s-metadata. This helps
                 provide superior System Monitoring.
               </p>
             </div>
             <div className="mt-5">
-              <h3 className="text-3xl font-semibold leading-tight">
+              <h3 className="text-3xl font-semibold leading-tight securitytitle">
                 Enforce security policies to containers in runtime
               </h3>
-              <p className="mt-2 font-light">
+              <p className="mt-2 font-light securitydes">
                 KubeArmor maintains security policies separately, which means that security policies
                 are no longer tightly coupled with containers. Then, KubeArmor directly applies the
                 security policies into Linux security modules (LSMs) for each container according to
--- src/components/landing/cover.js
+++ src/components/landing/cover.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import Button from '../Button';
-// import Img from 'gatsby-image';
-// import { useStaticQuery, graphql, Link } from 'gatsby';
+import Header from '../layout/Header';
+import Img from 'gatsby-image';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 
 const Cover = () => {
-  // const query = useStaticQuery(graphql`
-  //   query {
-  //     cover: file(relativePath: { eq: "landing/Coverpage.png" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid_withWebp
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+  const query = useStaticQuery(graphql`
+    query {
+      button: file(relativePath: { eq: "Header/Button.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
   return (
     <div className="covercontent">
+      <Header />
       <section className="pt-20 pb-20 md:pt-40 md:pb-40 ">
         <div className="container mx-auto px-8 lg:flex">
-          <div className="lg:w-1/2">
-            {/* <Img fluid={query.cover.childImageSharp.fluid} alt="Cover Page" /> */}
-          </div>
+          <div className="lg:w-1/2"></div>
           <div className="text-center lg:text-left lg:w-1/2">
+            <p>KubeArmor</p>
             <h2 className="text-4xl lg:text-5xl  leading-none covertitle">
               K8s-native System Policy Audit/Alert & Enforcement Engine
             </h2>
@@ -30,9 +31,25 @@ const Cover = () => {
               What if you “think” you don’t know enough about your topic? Then, do some research.
               Read books and magazines. Do some searches on the Internet. Who knows?
             </p>
-            <p className="mt-8 md:mt-12">
-              <Button size="lg">Find us on github</Button>
-            </p>
+            {/* <p className="mt-8 md:mt-12">
+              <Button size="lg">
+                Find us on github{' '}
+                <Img fluid={query.button.childImageSharp.fluid} alt="Cover Page" />
+              </Button>
+            </p> */}
+
+            <Button className="mt-8 md:mt-12" size="lg">
+              <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <div>Find us on github</div>
+                <div style={{ paddingLeft: '10px' }}>
+                  <Img
+                    fluid={query.button.childImageSharp.fluid}
+                    alt="Button Icon"
+                    className="Coverbutton"
+                  />
+                </div>
+              </div>
+            </Button>
           </div>
         </div>
       </section>
