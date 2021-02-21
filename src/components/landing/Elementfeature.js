c9efcf245816110e2cf916aa8fd5bc0b093f8664
Moghankumar <52576220+Moghankumar06@users.noreply.github.com>
2021-02-21 16:25:28 +0530

    Kubearmor changes

---
+++ src/components/landing/Elementfeature.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Elementfeature = () => {
+  return (
+    <div>
+      <section className="Elementfeatures pt-20 pb-20 md:pt-40 md:pb-40  ">
+        <section className="pt-20 pb-20 md:pt-40 md:pb-40"></section>
+      </section>
+    </div>
+  );
+};
+
+export default Elementfeature;
