3c3dc8af56b953e5d5d04d37c1c7f675af1d77b9
Moghankumar <52576220+Moghankumar06@users.noreply.github.com>
2021-03-03 12:30:32 +0530

    Changes

---
+++ src/components/Demopolicies/Demopolicies1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demopolicies1 = () => {
+  return (
+    <div className="pt-3 demopoliciestext">
+      <p>apiVersion: security.accuknox.com/v1</p>
+    </div>
+  );
+};
+
+export default Demopolicies1;
--- src/components/landing/Demopolicies.js
+++ src/components/landing/Demopolicies.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import { useStaticQuery, graphql } from 'gatsby';
+import Demopolicies1 from '../Demopolicies/Demopolicies1';
 
 const Demopolicies = () => {
   const query = useStaticQuery(graphql`
@@ -29,8 +30,11 @@ const Demopolicies = () => {
             KubeArmor Demo Policies
           </h1>
           <div className="row pt-5">
-            <div className="col-lg-12 col-md-12">
+            {/* <div className="col-lg-12 col-md-12">
               <Img fluid={query.demopolicies.childImageSharp.fluid} alt="Feature" />
+            </div> */}
+            <div className="col-lg-5 col-md-5 demopoliciescard">
+              <Demopolicies1 />
             </div>
           </div>
         </div>
