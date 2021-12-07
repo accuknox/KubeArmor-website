1ca01e530a907e8c11ff5d70d9890327cd1455f6
shubham <shubham@kloudlearn.com>
2021-12-07 20:32:19 +0530

    Feat(Header & Footer): banner and bottom line

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,229 +1,54 @@
-import React from 'react';
-// import Img from 'gatsby-image';
-// import { useStaticQuery, graphql } from 'gatsby';
+import React, { useEffect, useState } from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 
 const Footer = () => {
-  // const query = useStaticQuery(graphql`
-  //   query {
-  //     youtube: file(relativePath: { eq: "Footer/Youtube.png" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid_withWebp
-  //         }
-  //       }
-  //     }
-  //     twitter: file(relativePath: { eq: "Footer/Twitter.png" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid_withWebp
-  //         }
-  //       }
-  //     }
-  //     linkedin: file(relativePath: { eq: "Footer/Linkedin.png" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid_withWebp
-  //         }
-  //       }
-  //     }
-  //     facebook: file(relativePath: { eq: "Footer/Facebook.png" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid_withWebp
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+  const query = useStaticQuery(graphql`
+    query {
+      linkdin: file(relativePath: { eq: "Header/linkdin.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      github: file(relativePath: { eq: "Header/github.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
   return (
     <footer style={{ background: '#082D75' }}>
-      <div className="container mx-auto py-16 px-3 text-center text-white">
-        <p className="font-weight-bold" style={{ fontSize: '20px', lineHeight: '40px' }}>
+      <div className="container mx-auto pt-10 pb-2 text-center text-white">
+        <p style={{ fontSize: '20px', lineHeight: '40px' }}>
           KubeArmor is licensed under the Apache License, Version 2.0.
           <br />
           The eBPF-based container monitor is licensed under the General Public License, Version
           2.0.
         </p>
-        {/* <div className="flex -mx-3 text-white">
-          <div className="flex-1 text-white">
-            <h2 className="text-lg font-semibold">Platform</h2>
-            <ul className="mt-2 leading-loose text-white">
-              <li>
-                <a href="" className="text-sm text-white">
-                  Overview
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Features
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Integrations
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Mobile apps
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Pricing
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1 text-white">
-            <h2 className="text-lg font-semibold">Solutions</h2>
-            <ul className="mt-2 leading-loose">
-              <li>
-                <a href="" className="text-sm text-white">
-                  By training
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  By industry
-                </a>
-              </li>{' '}
-              <li>
-                <a href="" className="text-sm text-white">
-                  TalentLibrary
-                </a>
-              </li>{' '}
-              <li>
-                <a href="" className="text-sm text-white">
-                  Course providers
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1 ">
-            <h2 className="text-lg font-semibold">Learn</h2>
-            <ul className="mt-2 leading-loose">
-              <li>
-                <a href="" className="text-sm text-white">
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  eBooks
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Blended learning
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  What is an LMS?
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Best LMS
-                </a>
-              </li>
-            </ul>
-          </div>{' '}
-          <div className="flex-1">
-            <h2 className="text-lg font-semibold">Help</h2>
-            <ul className="mt-2 leading-loose">
-              <li>
-                <a href="" className="text-sm text-white">
-                  Help center
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  FAQ
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Privacy
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Security
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Terms of Service
-                </a>
-              </li>
-            </ul>
-          </div>{' '}
-          <div className="flex-1 ">
-            <h2 className="text-lg font-semibold">Company</h2>
-            <ul className="mt-2 leading-loose">
-              <li>
-                <a href="" className="text-sm text-white">
-                  About us
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Our customers
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Affiliates
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Customer success
-                </a>
-              </li>
-              <li>
-                <a href="" className="text-sm text-white">
-                  Contact us
-                </a>
-              </li>
-            </ul>
-          </div>
+      </div>
+      <div className="container mx-auto text-white">
+        <hr style={{ background: '#365F9E' }} />
+        <div className="row">
+          <p className="col-10" style={{ fontSize: '19px' }}>
+            © 2021 The KubeArmor Authors All Rights Reserved
+          </p>
+          <a
+            href="https://www.linkedin.com/company/kubearmor"
+            target="_blank"
+            className="col-1"
+            style={{ maxWidth: '5.33%' }}
+          >
+            <Img fluid={query.linkdin.childImageSharp.fluid} className="Coverbutton " />
+          </a>
+          <a href="https://github.com//kubearmor//KubeArmor" target="_blank" className="col-1">
+            <Img fluid={query.github.childImageSharp.fluid} className="Coverbutton" />
+          </a>
         </div>
-        <div className="text-center justify-between items-center">
-          <p className="text-white">© Epignosis</p>
-          <div className="footerlogocollection">
-            <div>
-              <Img
-                fluid={query.youtube.childImageSharp.fluid}
-                alt="Youtube"
-                className="footersociallogo"
-              />
-            </div>
-            <div>
-              <Img
-                fluid={query.twitter.childImageSharp.fluid}
-                alt="Youtube"
-                className="footersociallogo"
-              />
-            </div>
-            <div>
-              <Img
-                fluid={query.linkedin.childImageSharp.fluid}
-                alt="Youtube"
-                className="footersociallogo"
-              />
-            </div>
-            <div>
-              <Img
-                fluid={query.facebook.childImageSharp.fluid}
-                alt="Youtube"
-                className="footersociallogo"
-              />
-            </div>
-          </div>
-        </div> */}
       </div>
     </footer>
   );
