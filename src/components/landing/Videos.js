6cfa803dff37033b8b80ed4e0bbe892b2a25b0f6
abu hanzlaax <abu.hanzla@accuknox.com>
2022-09-21 16:12:13 +0530

    Videos about using Kubearmor on bottlerocket and Kubearmor on IoT edge - (KubeArmor Demo on AWS Bottlerocket ) - ( Open Horizon IoT Edge Security Demo )
    Update architecture diagram in line with what we have on the kubearmor GitHub repo

--- src/components/landing/Videos.js
+++ src/components/landing/Videos.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Img from 'gatsby-image';
-import { useStaticQuery, graphql, Link } from 'gatsby';
+import React from "react";
+import Img from "gatsby-image";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 const Videos = () => {
   const query = useStaticQuery(graphql`
@@ -25,17 +25,20 @@ const Videos = () => {
     <div>
       <section id="videos" className="pt-20 pb-10 bg-blue">
         <div className="container mx-auto">
-          <h1 className="font-weight-bold text-center pb-20" style={{ color: '#0B4296' }}>
+          <h1
+            className="font-weight-bold text-center pb-20"
+            style={{ color: "#0B4296" }}
+          >
             Videos
           </h1>
           <div className="row container mx-auto text-center justify-between items-center">
             <div className="col-lg-6 col-md-6">
               <div className="Videoplayer mx-auto">
                 <iframe
-                  style={{ borderRadius: '5px' }}
-                  width="90%"
-                  height="100%"
-                  src="https://www.youtube.com/embed/NS8XC78wSME"
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/esXWUDlU-ck"
+                  title="YouTube video player"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowfullscreen
@@ -45,7 +48,34 @@ const Videos = () => {
             <div className="col-lg-6 col-md-6">
               <div className="Videoplayer mx-auto">
                 <iframe
-                  style={{ borderRadius: '5px' }}
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/691AlW_GE1k"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6">
+              <div className="Videoplayer mx-auto">
+                <iframe
+                  style={{ borderRadius: "5px" }}
+                  width="90%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/NS8XC78wSME"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6">
+              <div className="Videoplayer mx-auto">
+                <iframe
+                  style={{ borderRadius: "5px" }}
                   width="90%"
                   height="100%"
                   src="https://www.youtube.com/embed/u1PpRsk03Ao"
@@ -56,7 +86,10 @@ const Videos = () => {
               </div>
             </div>
           </div>
-          <div className="row" style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <div
+            className="row"
+            style={{ alignItems: "center", justifyContent: "center" }}
+          >
             <a
               href="https://www.youtube.com/playlist?list=PLQjomRVn7MXC4obhiz1wuKLrGGip07HiM"
               target="_blank"
