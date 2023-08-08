cbfe78a7956ccf04a99401c17f2e5bc9c4933cec
Subham Ray <66999381+raysubham@users.noreply.github.com>
2023-08-09 00:07:28 +0530

    Revert "fix(Demo Policies): fixed the indentation and added copy button" (#61)

--- src/components/Demopolicies/Demopolicies1.js
+++ src/components/Demopolicies/Demopolicies1.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import CodeBlock from './CodeBlock';
-import { demoCode } from './demoPoliciesCode';
 
 const Demopolicies1 = () => {
   return (
-    <div className="p-2 demopoliciestext demopoliciescard" style={{ background: '#252937' }}>
-      <CodeBlock text={demoCode.code1} />
+    <div
+      className="pt-3 px-3 pb-3 demopoliciestext demopoliciescard"
+      style={{ background: '#252937' }}
+    >
+      <p style={{ marginBottom: '10px', color: 'white' }}>apiVersion: security.accuknox.com/v1</p>
+      <p style={{ marginBottom: '10px', color: 'white' }}>kind: KubeArmorPolicy</p>
+      <p style={{ marginBottom: '10px', color: 'white' }}>metadata: </p>
+      <p className="codetop">name: ksp-wordpress-config-block</p>
+      <p className="codetop">namespace: wordpress-mysql</p>
+      <p style={{ marginBottom: '10px', color: '#0AFF00' }}>spec:</p>
+      <p style={{ marginLeft: '15px', marginBottom: '10px', color: '#0AFF00' }}>severity: 10</p>
+      <p style={{ marginLeft: '15px', marginBottom: '10px', color: '#0AFF00' }}>selector:</p>
+      <p style={{ marginLeft: '30px', marginBottom: '10px', color: '#0AFF00' }}>matchLabels:</p>
+      <p style={{ marginLeft: '50px', marginBottom: '10px', color: '#0AFF00' }}>app: wordpress</p>
+      <p style={{ marginLeft: '15px', marginBottom: '10px', color: '#FFD100' }}>file:</p>
+      <p style={{ marginLeft: '30px', marginBottom: '10px', color: '#FFD100' }}>matchPaths:</p>
+      <p style={{ marginLeft: '30px', marginBottom: '10px', color: '#FFD100' }}>
+        - path: /var/www/html/wp-config.php
+      </p>
+      <p style={{ marginLeft: '45px', marginBottom: '10px', color: '#FFD100' }}>fromSource:</p>
+      <p style={{ marginLeft: '45px', marginBottom: '10px', color: '#FFD100' }}>
+        - path: /usr/sbin/apache2
+      </p>
+      <p style={{ marginLeft: '45px', marginBottom: '10px', color: '#FFD100' }}>action: Allow</p>
+      <p style={{ marginLeft: '30px', marginBottom: '10px', color: '#FFD100' }}>
+        - path: /var/www/html/wp-config.php
+      </p>
+      <p style={{ marginLeft: '45px', marginBottom: '10px', color: '#FFD100' }}>action: Block</p>
     </div>
   );
 };
--- src/components/Demopolicies/Demopolicies3.js
+++ src/components/Demopolicies/Demopolicies3.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import CodeBlock from './CodeBlock';
-import { demoCode } from './demoPoliciesCode';
 
 const Demopolicies3 = () => {
   return (
-    <div className="p-3 demopoliciestext demopoliciescard" style={{ background: '#252937' }}>
-      <CodeBlock text={demoCode.code2} />
+    <div
+      className="pt-3 px-3 pb-3 demopoliciestext demopoliciescard"
+      style={{ background: '#252937' }}
+    >
+      <p style={{ marginBottom: '11px', color: 'white' }}>apiVersion: security.accuknox.com/v1</p>
+      <p style={{ marginBottom: '11px', color: 'white' }}>kind: KubeArmorPolicy</p>
+      <p style={{ marginBottom: '11px', color: 'white' }}>metadata: </p>
+      <p className="codetop">name: ksp-wordpress-process-block</p>
+      <p className="codetop">namespace: wordpress-mysql</p>
+      <p style={{ marginBottom: '11px', color: '#0AFF00' }}>spec:</p>
+      <p style={{ marginLeft: '15px', marginBottom: '11px', color: '#0AFF00' }}>severity: 3</p>
+      <p style={{ marginLeft: '15px', marginBottom: '11px', color: '#0AFF00' }}>selector:</p>
+      <p style={{ marginLeft: '30px', marginBottom: '11px', color: '#0AFF00' }}>matchLabels:</p>
+      <p style={{ marginLeft: '50px', marginBottom: '11px', color: '#0AFF00' }}>app: wordpress</p>
+      <p style={{ marginLeft: '15px', marginBottom: '11px', color: '#FFD100' }}>process:</p>
+      <p style={{ marginLeft: '30px', marginBottom: '11px', color: '#FFD100' }}>matchPaths:</p>
+      <p style={{ marginLeft: '30px', marginBottom: '11px', color: '#FFD100' }}>
+        - path: /usr/bin/apt
+      </p>
+      <p style={{ marginLeft: '30px', marginBottom: '11px', color: '#FFD100' }}>
+        - path: /usr/bin/apt-get
+      </p>
+      <p style={{ marginLeft: '15px', marginBottom: '11px', color: '#FFD100' }}>action: Block</p>
     </div>
   );
 };
